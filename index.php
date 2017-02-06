e38fd7a2fc8740f98444d73b23318a411487ff67
David Kroll <curtain333-code@yahoo.com>
2017-02-05 23:46:34 -0500

    Added index.php and images folder plus an image.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Hello World</title>
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+
+<h1>Hello World</h1>
+
+<p><a href="http://www.krolldesign.net">KrollDesign.net</a></p>
+
+<img src="/images/texas_trees.jpg">
+
+</body>
+</html>
